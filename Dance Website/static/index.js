2747879a42d4e483b460216ac0146def23944d0f
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2023-03-07 12:42:09 +0530

    Add files via upload

---
+++ Dance Website/static/index.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $(".carousel").owlCarousel({
+//         // margin : 20,
+//         loop : true,
+//         autoplay : true,
+//         autoplayTimeout : 2000,
+//         autoplayHoverPause : true   
+//     })
+// })
